e82d8e5df60ab14c9fd5eee723594ded74e76585
Alondrob <alondrob@gmail.com>
2022-04-08 15:43:39 -0400

    12

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,45 @@
 class SessionsController < ApplicationController
+    skip_before_action :require_login, only: [:login, :auto_login]
 
-    def create
-        user = User.new(user_params)
-            if user.save && user.authenticate(user_params[:password])
-                token = encode_token({user_id: user.id})
-                render json: {user: UserSerializer.new(user).serializable_hash, token: token}, status: :created
-            else
-                render json: {error: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
-            end
+
+    # def create
+    #     user = User.new(user_params)
+    #         if user.save && user.authenticate(user_params[:password])
+    #             token = encode_token({user_id: user.id})
+    #             render json: {user: UserSerializer.new(user).serializable_hash, token: token}, status: :created
+    #         else
+    #             render json: {error: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    #         end
+    # end
+
+    def auto_login
+        if current_user
+            render json: {errorrs: "No user logged in."}
+        else
+            render json: {message: "You Are authorized"}
+        end
     end
+
+    def login
+        user = User.find_by(email: params["email"]).try(:authenticate, params["password"])
+        if user && user.authenticate(params[:password])
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {
+                user: user, 
+                jwt: token,
+                succesess: "Welcome Back, #{user.user_name}!"
+            }
+        else
+            render json: {
+                faliure: "Log in failed: there was an error"
+            }
+        end
+    end
+
+
+
+
 
     def user_params
         params.require(:user).permit(:email, :password)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,40 @@
 class UsersController < ApplicationController
 
 
-    def create
-        user = User.new(user_params)
-            if user.save && user.authenticate(user_params[:password])
-                token = encode_token({user_id: user.id})
-                render json: {user: UserSerializer.new(user).serializable_hash, token: token}, status: :created
-            else
-                render json: {error: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
-            end
+    # def create
+    #     user = User.new(user_params)
+    #         if user.save && user.authenticate(user_params[:password])
+    #             token = encode_token({user_id: user.id})
+    #             render json: {user: UserSerializer.new(user).serializable_hash, token: token}, status: :created
+    #         else
+    #             render json: {error: user.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    #         end
+    # end
+
+    def index
+        users = User.all
+        render json: users
     end
 
+    def create
+        user = User.create(user_params)
+        #binsing.pry
+        if user.valid?
+            payload = {user_id: user.id}
+            token = encode_token(payload)
+            render json: {
+                status: created,
+                user: user,
+                jwt: token
+            }
+        else
+            render json: {
+                status: 500,
+                errorrs: user.errors.full_messages
+            },
+            status: "not_acceptable"
+        end
+    end
 
     private 
 
